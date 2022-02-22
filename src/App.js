65a45629161aabfff2eae5016cd97e9a693ad7db
shafdev <shafik8011@gmail.com>
2022-02-22 23:55:51 +0530

    f4 commit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
       }}
       // "proxy":"https://www.nseindia.com",
     // const url3 = "https://api.sensibull.com/v1/events?";
-    const url2 = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY';
+    // const url2 = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY';
 
-    // const url3 = "/v1/events?";
+    const url3 = "/v1/events?";
     // const url2 = '/api/option-chain-indices?symbol=NIFTY';
     const getData = async () =>{
-      const fetchData = await fetch(url2, headers);
+      const fetchData = await fetch(url3, headers);
       const fetchjson = await fetchData.json();
       console.log(fetchjson);
     }
